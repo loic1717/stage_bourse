28983994b1d179735fcbecb45a8a66ef2f6e05f8
loic1717 <102816837+loic1717@users.noreply.github.com>
2023-05-31 14:17:54 +0200

    connexion,déconexion utilisateur

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ajout_utilisateur
+from .views import ajout_utilisateur,login_view,logout_view
 
 app_name = 'user'
 
 urlpatterns = [
     path('ajout/', ajout_utilisateur, name='ajout_utilisateur'),
+      path('login/', login_view, name='login'),
+    path('logout/',logout_view, name='logout'),
     # Ajoutez d'autres URL pour vos vues utilisateur
 ]
--- user/views.py
+++ user/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render, redirect
 from .forms import UtilisateurCreationForm
 from .models import UtilisateurModel
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 def ajout_utilisateur(request):
     form = UtilisateurCreationForm()
     if request.method == 'POST':
         form = UtilisateurCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('user/connection.html')  # Remplacez "nom_de_l_url_de_redirection" par le nom de l'URL de redirection après l'ajout réussi de l'utilisateur
+            return redirect('bourse/description.html')  # Remplacez "nom_de_l_url_de_redirection" par le nom de l'URL de redirection après l'ajout réussi de l'utilisateur
     return render(request, 'user/ajout_utilisateur.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('user/login.html')  # Redirige vers la page souhaitée après la connexion réussie
+        else:
+            error_message = "Invalid username or password."
+    else:
+        error_message = None
+
+    return render(request, 'user/connexion.html', {'error_message': error_message})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('user/connexion.html')  # Redirige vers la page de connexion après la déconnexion réussie
